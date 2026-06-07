96961737093d9a88c285e3d962a4bd5ec8ce947a
Adil Ghoghawala <93631183+adilghoghawala@users.noreply.github.com>
2026-06-06 21:24:59 -0500

    Add files via upload

--- src/data/players.js
+++ src/data/players.js
@@ -1,1 +1,222 @@
+export const PLAYERS = {
+  "1960s Lakers": [
+    { name: "Jerry West", pos: ["PG", "SG"], pts: 27.0, reb: 5.8, ast: 6.7, stl: 0.0, blk: 0.0 },
+    { name: "Elgin Baylor", pos: ["SF", "PF"], pts: 27.4, reb: 13.5, ast: 4.3, stl: 0.0, blk: 0.0 },
+    { name: "Rudy LaRusso", pos: ["PF", "C"], pts: 15.3, reb: 9.9, ast: 2.1, stl: 0.0, blk: 0.0 },
+  ],
+  "1960s Celtics": [
+    { name: "Bill Russell", pos: ["C"], pts: 15.1, reb: 22.5, ast: 4.3, stl: 0.0, blk: 3.5 },
+    { name: "John Havlicek", pos: ["SG", "SF"], pts: 20.8, reb: 6.3, ast: 4.8, stl: 0.0, blk: 0.0 },
+    { name: "Sam Jones", pos: ["PG", "SG"], pts: 18.9, reb: 4.9, ast: 3.5, stl: 0.0, blk: 0.0 },
+    { name: "Tom Heinsohn", pos: ["SF", "PF"], pts: 18.6, reb: 8.8, ast: 2.8, stl: 0.0, blk: 0.0 },
+  ],
+  "1960s Warriors": [
+    { name: "Wilt Chamberlain", pos: ["C"], pts: 37.6, reb: 22.9, ast: 5.0, stl: 0.0, blk: 4.0 },
+    { name: "Nate Thurmond", pos: ["C", "PF"], pts: 17.0, reb: 18.3, ast: 2.7, stl: 0.0, blk: 2.5 },
+    { name: "Guy Rodgers", pos: ["PG"], pts: 12.2, reb: 4.2, ast: 7.5, stl: 0.0, blk: 0.0 },
+  ],
+  "1970s Lakers": [
+    { name: "Kareem Abdul-Jabbar", pos: ["C"], pts: 27.7, reb: 14.5, ast: 3.6, stl: 0.9, blk: 3.5 },
+    { name: "Jerry West", pos: ["PG", "SG"], pts: 27.0, reb: 5.8, ast: 6.7, stl: 1.2, blk: 0.2 },
+    { name: "Gail Goodrich", pos: ["SG", "PG"], pts: 25.9, reb: 3.5, ast: 5.4, stl: 1.0, blk: 0.2 },
+    { name: "Happy Hairston", pos: ["PF"], pts: 13.6, reb: 12.0, ast: 1.8, stl: 0.5, blk: 0.3 },
+  ],
+  "1970s Knicks": [
+    { name: "Walt Frazier", pos: ["PG"], pts: 20.7, reb: 6.1, ast: 7.9, stl: 1.9, blk: 0.2 },
+    { name: "Earl Monroe", pos: ["SG"], pts: 18.8, reb: 3.6, ast: 4.4, stl: 1.3, blk: 0.2 },
+    { name: "Willis Reed", pos: ["C"], pts: 18.7, reb: 12.9, ast: 1.8, stl: 0.6, blk: 0.9 },
+    { name: "Bill Bradley", pos: ["SF", "PF"], pts: 12.4, reb: 3.2, ast: 3.4, stl: 0.8, blk: 0.2 },
+  ],
+  "1970s Celtics": [
+    { name: "Dave Cowens", pos: ["C", "PF"], pts: 18.2, reb: 13.6, ast: 4.0, stl: 1.1, blk: 0.9 },
+    { name: "John Havlicek", pos: ["SG", "SF"], pts: 20.8, reb: 6.3, ast: 4.8, stl: 1.5, blk: 0.2 },
+    { name: "Jo Jo White", pos: ["PG", "SG"], pts: 17.2, reb: 4.9, ast: 5.1, stl: 1.4, blk: 0.2 },
+    { name: "Paul Silas", pos: ["PF", "C"], pts: 9.4, reb: 11.3, ast: 2.0, stl: 1.5, blk: 0.6 },
+  ],
+  "1970s Warriors": [
+    { name: "Rick Barry", pos: ["SF", "SG"], pts: 29.4, reb: 6.2, ast: 4.2, stl: 2.1, blk: 0.3 },
+    { name: "Jamaal Wilkes", pos: ["SF"], pts: 14.2, reb: 7.3, ast: 2.0, stl: 1.2, blk: 0.4 },
+    { name: "Phil Smith", pos: ["SG", "PG"], pts: 16.2, reb: 3.0, ast: 4.4, stl: 1.5, blk: 0.3 },
+  ],
+  "1970s Bulls": [
+    { name: "Bob Love", pos: ["SF"], pts: 21.3, reb: 6.8, ast: 1.8, stl: 0.8, blk: 0.3 },
+    { name: "Norm Van Lier", pos: ["PG"], pts: 11.9, reb: 4.3, ast: 7.1, stl: 2.2, blk: 0.3 },
+    { name: "Chet Walker", pos: ["SF", "PF"], pts: 18.6, reb: 7.1, ast: 2.5, stl: 0.7, blk: 0.3 },
+  ],
+  "1980s Lakers": [
+    { name: "Magic Johnson", pos: ["PG"], pts: 19.5, reb: 7.2, ast: 11.2, stl: 1.9, blk: 0.4 },
+    { name: "Kareem Abdul-Jabbar", pos: ["C"], pts: 22.5, reb: 9.5, ast: 3.5, stl: 0.9, blk: 2.8 },
+    { name: "James Worthy", pos: ["SF", "PF"], pts: 19.6, reb: 7.4, ast: 3.8, stl: 1.5, blk: 0.7 },
+    { name: "Byron Scott", pos: ["SG"], pts: 14.8, reb: 3.4, ast: 3.0, stl: 1.4, blk: 0.3 },
+    { name: "Michael Cooper", pos: ["SF", "SG"], pts: 8.9, reb: 3.4, ast: 3.8, stl: 1.6, blk: 0.4 },
+  ],
+  "1980s Celtics": [
+    { name: "Larry Bird", pos: ["SF", "PF"], pts: 24.3, reb: 10.0, ast: 6.3, stl: 1.7, blk: 0.8 },
+    { name: "Robert Parish", pos: ["C"], pts: 17.6, reb: 10.9, ast: 2.0, stl: 1.1, blk: 1.5 },
+    { name: "Kevin McHale", pos: ["PF", "C"], pts: 18.3, reb: 8.5, ast: 2.1, stl: 0.7, blk: 1.7 },
+    { name: "Dennis Johnson", pos: ["PG", "SG"], pts: 14.1, reb: 4.0, ast: 5.8, stl: 1.7, blk: 0.4 },
+    { name: "Danny Ainge", pos: ["SG", "PG"], pts: 11.6, reb: 2.9, ast: 4.0, stl: 1.3, blk: 0.2 },
+  ],
+  "1980s 76ers": [
+    { name: "Julius Erving", pos: ["SF", "PF"], pts: 22.4, reb: 6.7, ast: 3.7, stl: 2.1, blk: 1.5 },
+    { name: "Moses Malone", pos: ["C"], pts: 24.5, reb: 12.9, ast: 1.3, stl: 0.9, blk: 1.7 },
+    { name: "Charles Barkley", pos: ["PF", "C"], pts: 23.0, reb: 11.7, ast: 3.9, stl: 1.5, blk: 0.8 },
+    { name: "Maurice Cheeks", pos: ["PG"], pts: 12.0, reb: 3.1, ast: 7.4, stl: 2.3, blk: 0.3 },
+    { name: "Andrew Toney", pos: ["SG", "PG"], pts: 15.9, reb: 2.7, ast: 5.0, stl: 1.4, blk: 0.2 },
+  ],
+  "1980s Bulls": [
+    { name: "Michael Jordan", pos: ["SG", "SF"], pts: 32.5, reb: 6.0, ast: 5.5, stl: 2.8, blk: 1.0 },
+    { name: "Scottie Pippen", pos: ["SF", "PF"], pts: 17.0, reb: 6.7, ast: 5.9, stl: 2.4, blk: 0.8 },
+    { name: "Horace Grant", pos: ["PF", "C"], pts: 12.8, reb: 8.9, ast: 2.8, stl: 1.3, blk: 0.7 },
+  ],
+  "1990s Bulls": [
+    { name: "Michael Jordan", pos: ["SG", "SF"], pts: 30.1, reb: 6.2, ast: 5.3, stl: 2.3, blk: 0.8 },
+    { name: "Scottie Pippen", pos: ["SF", "PF"], pts: 18.0, reb: 7.3, ast: 5.2, stl: 2.1, blk: 0.7 },
+    { name: "Dennis Rodman", pos: ["PF", "C"], pts: 5.7, reb: 13.1, ast: 2.5, stl: 0.7, blk: 0.4 },
+    { name: "Toni Kukoc", pos: ["SF", "PF"], pts: 13.1, reb: 4.3, ast: 4.1, stl: 1.1, blk: 0.5 },
+    { name: "Ron Harper", pos: ["PG", "SG"], pts: 9.3, reb: 3.2, ast: 3.2, stl: 1.4, blk: 0.4 },
+  ],
+  "1990s Jazz": [
+    { name: "Karl Malone", pos: ["PF"], pts: 25.7, reb: 10.4, ast: 3.2, stl: 1.3, blk: 0.6 },
+    { name: "John Stockton", pos: ["PG"], pts: 13.1, reb: 2.7, ast: 10.5, stl: 2.2, blk: 0.2 },
+    { name: "Jeff Hornacek", pos: ["SG"], pts: 14.5, reb: 3.8, ast: 4.9, stl: 1.4, blk: 0.2 },
+    { name: "Bryon Russell", pos: ["SF", "SG"], pts: 9.2, reb: 4.5, ast: 1.6, stl: 1.2, blk: 0.3 },
+  ],
+  "1990s Spurs": [
+    { name: "David Robinson", pos: ["C"], pts: 23.2, reb: 10.6, ast: 2.5, stl: 1.7, blk: 3.0 },
+    { name: "Tim Duncan", pos: ["C", "PF"], pts: 21.1, reb: 11.9, ast: 2.7, stl: 0.7, blk: 2.5 },
+    { name: "Avery Johnson", pos: ["PG"], pts: 10.5, reb: 2.6, ast: 6.8, stl: 1.5, blk: 0.1 },
+  ],
+  "1990s Suns": [
+    { name: "Charles Barkley", pos: ["PF", "C"], pts: 25.6, reb: 12.2, ast: 4.1, stl: 1.6, blk: 0.9 },
+    { name: "Kevin Johnson", pos: ["PG"], pts: 20.4, reb: 4.0, ast: 11.4, stl: 2.0, blk: 0.2 },
+    { name: "Dan Majerle", pos: ["SG", "SF"], pts: 13.6, reb: 4.3, ast: 2.9, stl: 1.4, blk: 0.3 },
+  ],
+  "2000s Lakers": [
+    { name: "Shaquille O'Neal", pos: ["C"], pts: 29.7, reb: 13.4, ast: 3.3, stl: 0.6, blk: 2.8 },
+    { name: "Kobe Bryant", pos: ["SG", "SF"], pts: 25.7, reb: 5.7, ast: 4.9, stl: 1.5, blk: 0.5 },
+    { name: "Pau Gasol", pos: ["PF", "C"], pts: 18.8, reb: 10.0, ast: 3.5, stl: 0.6, blk: 1.7 },
+    { name: "Derek Fisher", pos: ["PG"], pts: 8.9, reb: 2.3, ast: 3.2, stl: 0.8, blk: 0.1 },
+    { name: "Ron Artest", pos: ["SF", "SG"], pts: 14.0, reb: 4.3, ast: 2.5, stl: 1.9, blk: 0.4 },
+  ],
+  "2000s Spurs": [
+    { name: "Tim Duncan", pos: ["C", "PF"], pts: 22.0, reb: 12.4, ast: 3.2, stl: 0.7, blk: 2.4 },
+    { name: "Tony Parker", pos: ["PG"], pts: 18.9, reb: 3.1, ast: 7.7, stl: 1.2, blk: 0.1 },
+    { name: "Manu Ginobili", pos: ["SG", "SF"], pts: 16.9, reb: 4.3, ast: 4.4, stl: 1.6, blk: 0.3 },
+    { name: "Bruce Bowen", pos: ["SF", "SG"], pts: 6.5, reb: 3.7, ast: 1.5, stl: 1.6, blk: 0.5 },
+  ],
+  "2000s Pistons": [
+    { name: "Chauncey Billups", pos: ["PG", "SG"], pts: 17.3, reb: 3.0, ast: 5.7, stl: 1.1, blk: 0.2 },
+    { name: "Richard Hamilton", pos: ["SG"], pts: 17.1, reb: 3.6, ast: 3.5, stl: 1.0, blk: 0.2 },
+    { name: "Ben Wallace", pos: ["C", "PF"], pts: 6.9, reb: 13.2, ast: 1.7, stl: 1.5, blk: 2.4 },
+    { name: "Rasheed Wallace", pos: ["PF", "C"], pts: 14.5, reb: 7.5, ast: 2.0, stl: 1.0, blk: 1.3 },
+    { name: "Tayshaun Prince", pos: ["SF", "PF"], pts: 11.4, reb: 4.7, ast: 2.6, stl: 1.3, blk: 0.5 },
+  ],
+  "2000s Heat": [
+    { name: "Dwyane Wade", pos: ["SG", "PG"], pts: 27.2, reb: 5.8, ast: 7.1, stl: 2.0, blk: 0.9 },
+    { name: "Shaquille O'Neal", pos: ["C"], pts: 27.0, reb: 11.5, ast: 3.0, stl: 0.5, blk: 2.3 },
+    { name: "Alonzo Mourning", pos: ["C", "PF"], pts: 13.5, reb: 8.9, ast: 1.0, stl: 0.8, blk: 2.9 },
+  ],
+  "2010s Heat": [
+    { name: "LeBron James", pos: ["SF", "PF"], pts: 26.9, reb: 7.6, ast: 6.5, stl: 1.7, blk: 0.9 },
+    { name: "Dwyane Wade", pos: ["SG", "PG"], pts: 22.3, reb: 4.8, ast: 5.4, stl: 1.7, blk: 0.7 },
+    { name: "Chris Bosh", pos: ["PF", "C"], pts: 18.0, reb: 7.4, ast: 1.8, stl: 0.7, blk: 0.8 },
+    { name: "Ray Allen", pos: ["SG"], pts: 12.1, reb: 2.8, ast: 1.6, stl: 0.8, blk: 0.2 },
+    { name: "Mario Chalmers", pos: ["PG"], pts: 9.0, reb: 2.4, ast: 3.7, stl: 1.3, blk: 0.2 },
+  ],
+  "2010s Warriors": [
+    { name: "Stephen Curry", pos: ["PG"], pts: 25.3, reb: 4.5, ast: 6.4, stl: 1.7, blk: 0.2 },
+    { name: "Kevin Durant", pos: ["SF", "PF"], pts: 27.1, reb: 7.4, ast: 5.1, stl: 1.1, blk: 1.1 },
+    { name: "Klay Thompson", pos: ["SG", "SF"], pts: 20.1, reb: 3.8, ast: 2.2, stl: 1.0, blk: 0.4 },
+    { name: "Draymond Green", pos: ["PF", "C"], pts: 11.1, reb: 8.2, ast: 7.1, stl: 1.5, blk: 1.3 },
+    { name: "Andre Iguodala", pos: ["SF", "SG"], pts: 8.9, reb: 4.2, ast: 3.4, stl: 1.4, blk: 0.4 },
+  ],
+  "2010s Cavaliers": [
+    { name: "LeBron James", pos: ["SF", "PF"], pts: 27.5, reb: 8.5, ast: 7.4, stl: 1.7, blk: 0.7 },
+    { name: "Kyrie Irving", pos: ["PG", "SG"], pts: 21.1, reb: 3.6, ast: 5.2, stl: 1.2, blk: 0.3 },
+    { name: "Kevin Love", pos: ["PF", "C"], pts: 16.0, reb: 9.9, ast: 2.4, stl: 0.7, blk: 0.5 },
+    { name: "J.R. Smith", pos: ["SG", "SF"], pts: 12.5, reb: 3.0, ast: 1.5, stl: 0.8, blk: 0.2 },
+  ],
+  "2010s Spurs": [
+    { name: "Tim Duncan", pos: ["C", "PF"], pts: 17.7, reb: 9.8, ast: 3.0, stl: 0.7, blk: 1.9 },
+    { name: "Tony Parker", pos: ["PG"], pts: 17.5, reb: 2.7, ast: 6.9, stl: 0.9, blk: 0.1 },
+    { name: "Manu Ginobili", pos: ["SG", "SF"], pts: 13.6, reb: 3.5, ast: 3.9, stl: 1.5, blk: 0.3 },
+    { name: "Kawhi Leonard", pos: ["SF", "SG"], pts: 19.1, reb: 6.5, ast: 2.7, stl: 2.0, blk: 0.7 },
+    { name: "LaMarcus Aldridge", pos: ["PF", "C"], pts: 19.3, reb: 8.6, ast: 2.3, stl: 0.6, blk: 1.1 },
+  ],
+  "2010s Thunder": [
+    { name: "Kevin Durant", pos: ["SF", "PF"], pts: 29.1, reb: 7.4, ast: 3.9, stl: 1.4, blk: 1.0 },
+    { name: "Russell Westbrook", pos: ["PG"], pts: 23.5, reb: 5.5, ast: 7.3, stl: 1.7, blk: 0.2 },
+    { name: "James Harden", pos: ["SG", "SF"], pts: 17.8, reb: 4.1, ast: 3.7, stl: 1.4, blk: 0.5 },
+  ],
+  "2020s Lakers": [
+    { name: "LeBron James", pos: ["SF", "PF"], pts: 27.1, reb: 7.4, ast: 7.8, stl: 1.3, blk: 0.5 },
+    { name: "Anthony Davis", pos: ["C", "PF"], pts: 25.9, reb: 11.2, ast: 2.6, stl: 1.3, blk: 2.3 },
+    { name: "Kyle Kuzma", pos: ["PF", "SF"], pts: 12.8, reb: 6.1, ast: 1.7, stl: 0.7, blk: 0.4 },
+  ],
+  "2020s Bucks": [
+    { name: "Giannis Antetokounmpo", pos: ["PF", "C"], pts: 28.1, reb: 11.0, ast: 5.9, stl: 1.1, blk: 1.4 },
+    { name: "Khris Middleton", pos: ["SF", "SG"], pts: 20.4, reb: 5.4, ast: 5.5, stl: 1.2, blk: 0.3 },
+    { name: "Jrue Holiday", pos: ["PG", "SG"], pts: 17.0, reb: 4.5, ast: 6.8, stl: 1.6, blk: 0.3 },
+    { name: "Brook Lopez", pos: ["C"], pts: 14.4, reb: 5.0, ast: 2.2, stl: 0.7, blk: 1.6 },
+  ],
+  "2020s Celtics": [
+    { name: "Jayson Tatum", pos: ["SF", "PF"], pts: 27.0, reb: 8.3, ast: 4.7, stl: 1.1, blk: 0.6 },
+    { name: "Jaylen Brown", pos: ["SG", "SF"], pts: 22.7, reb: 5.5, ast: 3.5, stl: 1.2, blk: 0.4 },
+    { name: "Jrue Holiday", pos: ["PG", "SG"], pts: 13.5, reb: 4.6, ast: 4.8, stl: 1.6, blk: 0.4 },
+    { name: "Al Horford", pos: ["C", "PF"], pts: 11.9, reb: 6.7, ast: 3.4, stl: 0.8, blk: 1.2 },
+    { name: "Kristaps Porzingis", pos: ["C", "PF"], pts: 20.1, reb: 7.2, ast: 2.0, stl: 0.6, blk: 1.9 },
+  ],
+  "2020s Nuggets": [
+    { name: "Nikola Jokic", pos: ["C"], pts: 26.4, reb: 11.8, ast: 9.0, stl: 1.3, blk: 0.7 },
+    { name: "Jamal Murray", pos: ["PG", "SG"], pts: 21.2, reb: 4.1, ast: 6.5, stl: 1.1, blk: 0.3 },
+    { name: "Michael Porter Jr.", pos: ["SF", "PF"], pts: 16.7, reb: 7.0, ast: 1.7, stl: 0.9, blk: 0.5 },
+    { name: "Aaron Gordon", pos: ["PF", "SF"], pts: 13.9, reb: 6.5, ast: 3.4, stl: 0.9, blk: 0.5 },
+  ],
+};
 
+export const TEAM_ERAS = Object.keys(PLAYERS);
+export const POSITIONS = ["PG", "SG", "SF", "PF", "C"];
+
+export const THRESHOLDS = {
+  pts: 115,
+  reb: 47,
+  ast: 26,
+  stl: 6.5,
+  blk: 4.5,
+};
+
+export function playerRating(p) {
+  return +(p.pts * 0.35 + p.reb * 0.22 + p.ast * 0.20 + p.stl * 1.2 + p.blk * 1.1).toFixed(1);
+}
+
+export function simulate(roster) {
+  const players = Object.values(roster);
+  const totals = { pts: 0, reb: 0, ast: 0, stl: 0, blk: 0 };
+  players.forEach(p => {
+    totals.pts += p.pts;
+    totals.reb += p.reb;
+    totals.ast += p.ast;
+    totals.stl += p.stl;
+    totals.blk += p.blk;
+  });
+
+  const failed = Object.keys(THRESHOLDS).filter(k => totals[k] < THRESHOLDS[k]);
+  let wins = 38;
+  failed.forEach(k => {
+    wins -= Math.round((THRESHOLDS[k] - totals[k]) / THRESHOLDS[k] * 18);
+  });
+  wins = Math.max(0, Math.min(38, wins));
+  const losses = 38 - wins;
+
+  return { totals, failed, wins, losses };
+}
+
+export function getGrade(wins) {
+  if (wins === 38) return { record: "38-0", grade: "Dynasty", flavor: "Unstoppable. A perfect playoff run for the ages.", color: "text-yellow-400" };
+  if (wins >= 35) return { record: `${wins}-${38 - wins}`, grade: "Elite", flavor: "One of the greatest runs ever. Near-perfect.", color: "text-green-400" };
+  if (wins >= 30) return { record: `${wins}-${38 - wins}`, grade: "Contender", flavor: "Strong enough to win a title on a good night.", color: "text-blue-400" };
+  if (wins >= 24) return { record: `${wins}-${38 - wins}`, grade: "Dangerous", flavor: "Solid, but flaws will catch up eventually.", color: "text-orange-400" };
+  if (wins >= 16) return { record: `${wins}-${38 - wins}`, grade: "Inconsistent", flavor: "Big names, big flaws. First-round upset incoming.", color: "text-red-400" };
+  return { record: `${wins}-${38 - wins}`, grade: "Historic... ly bad", flavor: "Somehow worse than the 2012 Bobcats.", color: "text-red-600" };
+}
